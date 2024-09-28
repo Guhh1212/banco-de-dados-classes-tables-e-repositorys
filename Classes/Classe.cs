4b0495e19bde87a2e27c2b56b7ba2cd046425b5f
Gustavo Godoy <150524617+Guhh1212@users.noreply.github.com>
2024-09-28 18:13:37 -0300

    Add files via upload

---
+++ Classes/Classe.cs
@@ -0,0 +1,14 @@
+public funcionarios {
+	public int Id { get; set; }
+	public string funcionarios { get; set; }	
+}
+
+public veiculos {
+	public int Id { get; set; }
+	public string veiculos { get; set; }	
+}
+
+public salarios {
+	public int Id { get; set; }
+	public string salarios { get; set; }	
+}
---
+++ Repositorys/Repository.cs
@@ -0,0 +1,14 @@
+public async Task<IList<Funcionario>> GetAllFuncionario()
+{
+		var query = "Funcionario"
+}
+
+public async Task<IList<Veiculos>> GetAllVeiculos()
+{
+		var query = "Veiculos"
+}
+
+public async Task<IList<Salarios>> GetAllSalarios()
+{
+		var query = "Salarios"
+}
